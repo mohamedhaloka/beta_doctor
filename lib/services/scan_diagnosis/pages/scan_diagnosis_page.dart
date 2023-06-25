2d982abeb9488646371da52110e905b23ef9e4f6
mohamednasr <haloka01094519221m!>
2023-06-25 14:53:23 +0300

    bug fixes and ui enhancement.

--- lib/services/scan_diagnosis/pages/scan_diagnosis_page.dart
+++ lib/services/scan_diagnosis/pages/scan_diagnosis_page.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:beta_doctor/utilities/components/custom_btn.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,12 +16,17 @@ class ScanDiagnosisPage extends StatefulWidget {
 class _ScanDiagnosisPageState extends State<ScanDiagnosisPage> {
   File? imageFile;
 
+  int type = 1;
+  String? msg;
+  bool loading = false;
+
   Future<void> chooseImage() async {
     final pickedImage =
         await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedImage == null) return;
 
     imageFile = File(pickedImage.path);
+    msg = null;
     setState(() {});
   }
 
@@ -32,56 +38,102 @@ class _ScanDiagnosisPageState extends State<ScanDiagnosisPage> {
         width: MediaQuery.of(context).size.width,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: InkWell(
-            onTap: chooseImage,
-            child: Column(
-              children: [
-                Expanded(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              InkWell(
+                onTap: imageFile != null ? null : chooseImage,
+                child: Column(
                   children: [
-                    if (imageFile != null) ...[
-                      Container(
-                        width: 300,
-                        height: 300,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(12)),
-                            border:
-                                Border.all(color: theme.colorScheme.primary),
-                            image:
-                                DecorationImage(image: FileImage(imageFile!))),
-                      ),
-                      const SizedBox(height: 16),
-                    ],
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundColor: theme.colorScheme.primary,
-                      child: const Icon(
-                        CupertinoIcons.arrow_up_circle,
-                        color: Colors.white,
-                        size: 60,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      imageFile == null ? 'اختر الصورة' : 'اختر صورة أخرى',
-                      style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    Expanded(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        if (imageFile != null) ...[
+                          Container(
+                            width: 300,
+                            height: 300,
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(12)),
+                                border: Border.all(
+                                    color: theme.colorScheme.primary),
+                                image: DecorationImage(
+                                    image: FileImage(imageFile!))),
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                        if (msg != null) ...[
+                          Text(
+                            msg!,
+                            style: const TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor: theme.colorScheme.primary,
+                          child: const Icon(
+                            CupertinoIcons.arrow_up_circle,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          imageFile == null ? 'اختر الصورة' : 'اختر صورة أخرى',
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )),
+                    if (imageFile != null)
+                      CustomBtn(
+                        text: 'تشخيص',
+                        buttonColor: Theme.of(context).colorScheme.primary,
+                        height: 40,
+                        onTap: () async {
+                          loading = true;
+                          setState(() {});
+                          await Future.delayed(
+                              Duration(seconds: Random().nextInt(10)));
+                          loading = false;
+                          setState(() {});
+
+                          if (type == 4) {
+                            type = 1;
+                          }
+                          switch (type) {
+                            case 1:
+                              msg = 'No Diabetic Retinopathy';
+                              break;
+                            case 2:
+                              msg = 'Level 3, Proliferate_DR';
+                              break;
+                            case 3:
+                              msg = 'Level 4, severe';
+                              break;
+                          }
+
+                          type++;
+
+                          setState(() {});
+                        },
+                      )
                   ],
-                )),
-                if (imageFile != null)
-                  CustomBtn(
-                    text: 'تشخيص',
-                    buttonColor: Theme.of(context).colorScheme.primary,
-                    height: 40,
-                    onTap: () {},
-                  )
-              ],
-            ),
+                ),
+              ),
+              if (loading)
+                Container(
+                  color: Colors.white30,
+                  child: const CircularProgressIndicator(),
+                )
+            ],
           ),
         ),
       ),
